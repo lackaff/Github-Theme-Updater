e01cf5a5b808f81054d0988d3375a77070f98f25
Andy Fragen <andy@thefragens.com>
2012-12-31 19:33:35 -0800

    more fixes for autoupdating

--- github-theme-updater.php
+++ github-theme-updater.php
@@ -1,11 +1,12 @@
 <?php
 /*
-Plugin Name: Theme Updater
-Plugin URI: https://github.com/UCF/Theme-Updater
-Description: A theme updater for GitHub hosted Wordpress themes.  This Wordpress plugin automatically checks GitHub for theme updates and enables automatic install.  For more information read <a href="https://github.com/UCF/Theme-Updater/blob/master/readme.markdown">plugin documentation</a>.
+Plugin Name: Theme Updater - Revised
+Plugin URI: https://github.com/afragen/Github-Theme-Updater
+Description: A theme updater for GitHub hosted Wordpress themes.  This Wordpress plugin automatically checks GitHub for theme updates and enables automatic install.  For more information read <a href="https://github.com/UCF/Theme-Updater/blob/master/readme.markdown">plugin documentation</a>. Sadly this plugin doesn't look like it's actively supported.
 Author: Douglas Beck
 Author: UCF Web Communications
-Version: 1.3.7
+Original Vers: 1.3.7
+Version: 1.4
 
 This is dummy to load original files and use for automatic updating.
 */
@@ -17,7 +18,7 @@ define('GTU_INCLUDES', GTU_DIR.'/includes');
 //define('GTU_VIEWS', GTU_DIR.'/views');
 //define('GTU_RESOURCES', plugin_dir_url(__FILE__).'resources');
 define('GTU_INC', GTU_DIR.'/inc');
-define('GTU_GIT', 'Github-Theme-Updater' );
+define('GIT_REPO', 'Github-Theme-Updater' );
 
 //Load base class
 require_once( GTU_INC.'/gtu-updater.php' );
@@ -29,10 +30,10 @@ if ( is_admin() ) {
 	$config = array(		
 		'slug' => plugin_basename(__FILE__),
 		'proper_folder_name' => dirname( plugin_basename(__FILE__) ),
-		'api_url' => 'https://api.github.com/repos/afragen/'.GTU_GIT,
-		'raw_url' => 'https://raw.github.com/afragen/'.GTU_GIT.'/master',
-		'github_url' => 'https://github.com/afragen/'.GTU_GIT,
-		'zip_url' => 'https://github.com/afragen/'.GTU_GIT.'/zipball/master',
+		'api_url' => 'https://api.github.com/repos/afragen/'.GIT_REPO,
+		'raw_url' => 'https://raw.github.com/afragen/'.GIT_REPO.'/master',
+		'github_url' => 'https://github.com/afragen/'.GIT_REPO,
+		'zip_url' => 'https://github.com/afragen/'.GIT_REPO.'/zipball/master',
 		'sslverify' => true,
 		'requires' => $wp_version,
 		'tested' => $wp_version,
