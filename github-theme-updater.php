09debaaed1b995897f305c15ccffec4f12243d98
Andy Fragen <andy@thefragens.com>
2013-01-03 21:19:36 -0800

    test GU refactor

--- github-theme-updater.php
+++ github-theme-updater.php
@@ -7,7 +7,7 @@ Author: Andy Fragen
 Original Author: Douglas Beck
 Original Author: UCF Web Communications
 Original Vers: 1.3.7
-Version: 1.5.7.1
+Version: 1.5.7.2
 
 This is dummy to load original files and use for automatic updating.
 */
@@ -16,4 +16,22 @@ This is dummy to load original files and use for automatic updating.
 require_once( 'updater.php' );
 
 //Load GithubUpdater
-require_once( dirname(__FILE__).'/updater/updater-settings.php');
+if ( is_admin() ) {
+	$repo = 'afragen/Github-Theme-Updater';
+	global $wp_version;
+	include_once( dirname(__FILE__).'/updater/updater.php' );
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => basename($repo),
+			'api_url' => 'https://api.github.com/repos/'.$repo,
+			'raw_url' => 'https://raw.github.com/'.$repo.'/master',
+			'github_url' => 'https://github.com/'.$repo,
+			'zip_url' => 'https://github.com/'.$repo.'/zipball/master',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'readme.txt',
+			'access_token' => '',
+		);
+	new WP_GitHub_Updater($config);
+}
