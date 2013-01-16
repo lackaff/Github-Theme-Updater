7c843c5851874d80e20ce93d5a626f80b28efcd9
Andy Fragen <andy@thefragens.com>
2013-01-16 09:43:18 -0800

    temporary fix for PHP Fatal error with WP_Error

--- github-theme-updater.php
+++ github-theme-updater.php
@@ -7,7 +7,7 @@ Author: Andy Fragen
 Original Author: Douglas Beck
 Original Author: UCF Web Communications
 Original Vers: 1.3.7
-Version: 1.5.7.3
+Version: 1.5.7.4
 
 This is dummy to load original files and use for automatic updating.
 */
